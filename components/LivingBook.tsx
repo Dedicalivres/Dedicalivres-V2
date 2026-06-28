a01a3977ac56330c1a4f4aea898c47b2ef1f7bea
Dedicalivres <dedicalivres@gmail.com>
2026-06-28 18:31:43 +0200

    Add files via upload

--- components/LivingBook.tsx
+++ components/LivingBook.tsx
@@ -2,17 +2,19 @@
 
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
-import { LivingBookFallback } from "./LivingBookFallback";
+import { LivingBookFallback } from "@/components/LivingBookFallback";
 
 const GrimoireVivant = dynamic(
-  () => import("./GrimoireVivant").then((mod) => ({ default: mod.GrimoireVivant })),
+  () => import("@/components/GrimoireVivant").then((mod) => ({ default: mod.GrimoireVivant })),
   { ssr: false, loading: () => <LivingBookFallback /> }
 );
 
 export function LivingBook() {
   return (
-    <Suspense fallback={<LivingBookFallback />}>
-      <GrimoireVivant />
-    </Suspense>
+    <div className="grimoire-stage" aria-hidden="false">
+      <Suspense fallback={<LivingBookFallback />}>
+        <GrimoireVivant />
+      </Suspense>
+    </div>
   );
 }
